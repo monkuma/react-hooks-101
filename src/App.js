118c037f7e19cb1796349e7178e1938723ff7196
monkuma <kumamoto.jobs@gmail.com>
2022-10-18 20:42:17 +0900

    implement component using multiple states

--- src/App.js
+++ src/App.js
@@ -1,51 +1,29 @@
 import React, { useState } from "react";
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const App = (props) => {
+  const [name, setName] = useState(props.name);
+  const [price, setPrice] = useState(props.price);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-  const decrement = () => setCount(count - 1);
-
-  const increment2 = () => setCount((prev) => prev + 1);
-  const decrement2 = () => setCount((prev) => prev - 1);
-
-  // const reset = () => setCount((prev) => prev - prev);
-  const reset = () => setCount(0);
-
-  const double = () => setCount((prev) => prev * 2);
-
-  const divide3 = () => {
-    setCount((prev) => {
-      const newPrev = prev;
-      if (newPrev % 3 === 0) {
-        return newPrev / 3;
-      } else {
-        return prev;
-      }
-    });
+  const reset = () => {
+    setPrice(props.price);
+    setName(props.name);
   };
   return (
     <>
-      <div>
-        <h1>this is a template for React App.</h1>
-        <p>Count {count}</p>
-        <button onClick={increment}>+1</button>
-        <button onClick={decrement}>-1</button>
-      </div>
-      <div>
-        <button onClick={increment2}>+1</button>
-        <button onClick={decrement2}>-1</button>
-      </div>
-
-      <div>
-        <button onClick={reset}>RESET</button>
-        <button onClick={double}>*2</button>
-        <button onClick={divide3}>divide3</button>
-      </div>
+      <p>
+        現在の{name}は、{price}えんです。
+      </p>
+      <button onClick={() => setPrice(price + 1)}>+1</button>
+      <button onClick={() => setPrice(price - 1)}>-1</button>
+      <button onClick={reset}>reset</button>
+      <input value={name} onChange={(e) => setName(e.target.value)} />
     </>
   );
+};
+
+App.defaultProps = {
+  name: "Sample",
+  price: 1000,
 };
 
 export default App;
